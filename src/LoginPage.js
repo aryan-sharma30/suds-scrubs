504d3d951e0c8dc732f22444cd4bacadd2bd8a5f
aryan-sharma30 <80668347+aryan-sharma30@users.noreply.github.com>
2024-10-29 16:12:23 +0000

    feat: BE link

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -14,7 +14,7 @@ const LoginPage = () => {
     setLoading(true); // Start loading
 
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', {
+      const response = await fetch('https://suds-scrubs-production.up.railway.app/api/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ phone, password }),
